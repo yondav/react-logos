2e370e181483c1742ca79cc931fff59340c035b3
yondav <yonidavid.abr@gmail.com>
2021-07-29 00:53:47 -0400

    google alt component

--- react-logos/src/lib/components/Google/index.js
+++ react-logos/src/lib/components/Google/index.js
@@ -6,6 +6,7 @@ export { default as AndroidAlt2 } from './Android_Alt2';
 export { default as Firebase } from './Firebase';
 export { default as Gmail } from './Gmail';
 export { default as Google } from './Google';
+export { default as GoogleAlt } from './Google_Alt';
 export { default as Google360 } from './Google_360';
 export { default as GoogleAdMob } from './Google_AdMob';
 export { default as GoogleAds } from './Google_Ads';
